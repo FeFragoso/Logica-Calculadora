ad072d421999f58c976fb879a60e760b339e2372
fefragoso <felipeofragoso@gmail.com>
2022-08-01 01:01:19 -0300

    Adição do botão excluir

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ function backspace(){
     x = document.getElementById('visor').innerHTML
     document.getElementById('visor').innerHTML = x.substring(0, x.length -1)
 }
+
+function excluir(){
+    document.getElementById('visor').innerHTML = ''
+}
